03e15f6daa6c8b3adbd19c3b9f1256e9b3290a68
Maira Cavalheiro <mairadanielec@gmail.com>
2018-05-07 22:02:37 -0400

    Initial Commit

---
+++ template-teste/footer.php
@@ -0,0 +1,75 @@
+<div class="footer">
+	<div class="container w-container">
+		<div class="footer-row w-row">
+			<div class="footer-column w-col w-col-3">
+				<div class="footer-title">Contact us</div>
+				<a class="footer-contact-block w-inline-block" href="#">
+					<div class="footer-contact-title icon"></div>
+					<div class="footer-contact-title">+1 303 4020 43</div></a>
+					<a class="footer-contact-block w-inline-block" href="#">
+						<div class="footer-contact-title icon"></div>
+						<div class="footer-contact-title">+1 303 4020 42</div>
+					</a>
+					<a class="footer-contact-block w-inline-block" href="#">
+						<div class="footer-contact-title icon"></div>
+						<div class="footer-contact-title">info@university.com</div>
+					</a>
+					<a class="footer-social-button w-inline-block" href="http://www.facebook.com" target="_blank">
+						<img class="footer-social-icon" src="http://uploads.webflow.com/571cab5c86c1049c4c0e7047/5725bf52138ce8c852415122_Icon-facebook_2.png"></a>
+						<a class="footer-social-button w-inline-block" href="http://www.twitter.com" target="_blank">
+							<img class="footer-social-icon" src="http://uploads.webflow.com/571cab5c86c1049c4c0e7047/5725bf7e639cfa231e7840b7_Icon-twitter_1.png"></a>
+							<a class="footer-social-button w-inline-block" href="http://www.linkedin.com" target="_blank">
+								<img class="footer-social-icon" src="http://uploads.webflow.com/571cab5c86c1049c4c0e7047/5725bf9f138ce8c852415257_Icon-linkedin.png"></a>
+							</div>
+							<div class="footer-column last w-col w-col-3">
+								<div class="footer-title">About us</div>
+								<a class="footer-list-link" href="/about-us">About us</a>
+								<a class="footer-list-link" href="/404">Our Teachers</a>
+								<a class="footer-list-link" href="/faqs">FAQs</a>
+								<a class="footer-list-link" href="/contact-us">Contact us</a>
+								<a class="footer-list-link" href="/404">Helpdesk</a>
+							</div>
+							<div class="footer-column w-col w-col-3">
+								<div class="footer-title">USEFUL LINKS</div>
+								<a class="footer-list-link" href="/faqs">FAQs</a>
+								<a class="footer-list-link" href="/contact-us">Contact us</a>
+								<a class="footer-list-link" href="/404">Helpdesk</a>
+								<a class="footer-list-link" href="/404">Our Teachers</a>
+								<a class="footer-list-link" href="/about-us">About us</a>
+							</div>
+							<div class="footer-column w-col w-col-3">
+								<div class="footer-title">FEATURED COURSES</div>
+								<div class="w-dyn-list">
+									<div class="w-dyn-items">
+										<div class="w-dyn-item">
+											<a class="footer-list-link" href="/courses/beginner-study-english-language">Beginner study English language</a>
+										</div>
+										<div class="w-dyn-item"><a class="footer-list-link" href="/courses/computer-science">Computer science beginner class</a>
+										</div>
+										<div class="w-dyn-item">
+											<a class="footer-list-link" href="/courses/architecture-building-and-planning">Architecture, building and planning</a>
+										</div>
+										<div class="w-dyn-item">
+											<a class="footer-list-link" href="/courses/politics-philosophy-and-theology">Politics, philosophy and theology</a>
+										</div>
+										<div class="w-dyn-item">
+											<a class="footer-list-link" href="/courses/medicine-and-dentistry">Medicine and dentistry professional</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="bottom-footer-block">
+							<div class="bottom-footer-text">Copyright © University&nbsp;- Images used for demonstration purpose only (<a class="footer-link" href="/404">Licenses</a>)
+							</div>
+							<div class="bottom-footer-text right">Template design by&nbsp;
+								<a class="footer-link" target="_blank" href="http://studiocorvus.com/">Studio Corvus</a>&nbsp;-&nbsp;
+								<a class="footer-link" target="_blank" href="https://webflow.com/templates/designers/rowan-hartsuiker">View all templates</a>
+							</div>
+						</div>
+					</div>
+				</div>
+
+     
+</body>
+</html>
---
+++ template-teste/header.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html><!-- This site was created in Webflow. http://www.webflow.com--><!-- Last Published: Mon Sep 12 2016 06:31:38 GMT+0000 (UTC) --><html data-wf-domain="template-university.webflow.io" data-wf-page="57278920639cfa231e7a9578" data-wf-site="571cab5c86c1049c4c0e7047">
+<head>
+	<meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>>
+	<title><?php wp_title(''); ?></title>
+	<meta content="Template University - Webflow Responsive Website Template" name="description">
+	<meta content="width=device-width, initial-scale=1" name="viewport">
+	<meta content="Webflow" name="generator">
+	
+	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+	<script src="https://ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js"></script>
+	<script type="text/javascript">WebFont.load({
+  google: {
+    families: ["Source Sans Pro:200,300,regular,italic,600,700,900"]
+  }
+});</script>
+<script src="https://daks2k3a4ib2z.cloudfront.net/0globals/modernizr-2.7.1.js" type="text/javascript"></script>
+<link href="https://daks2k3a4ib2z.cloudfront.net/571cab5c86c1049c4c0e7047/572660eb138ce8c8524203a9_Favicon.png" rel="shortcut icon" type="image/x-icon">
+<link href="https://daks2k3a4ib2z.cloudfront.net/571cab5c86c1049c4c0e7047/572660ed9134bc281e40be6a_Webclip.png" rel="apple-touch-icon">
+</head>
+ <body>
+ 	<div class="header-wrapper">
+	<div class="top-navbar w-nav" data-animation="default" data-collapse="none" data-contain="1" data-duration="400">
+	<div class="w-container">
+		<nav class="top-nav-menu w-nav-menu" role="navigation">
+		<a class="top-nav-link w-inline-block" href="#">
+		<div class="icon top-nav-title"></div>
+		<div class="top-nav-title">+1 303 4020 43</div></a>
+		<a class="top-nav-link w-inline-block" href="#">
+			<div class="icon top-nav-title"></div>
+			<div class="top-nav-title">info@university.com</div></a></nav>
+			<div class="top-bar-text w-hidden-small w-hidden-tiny">Weekly new courses!</div>
+			<a class="top-bar-link w-hidden-small w-hidden-tiny" href="/our-courses/courses-featured">View Featured Courses&nbsp;→</a>
+			<div class="w-nav-button">
+				<div class="w-icon-nav-menu"></div>
+			</div>
+		</div>
+		</div>
+		<div class="navbar w-nav" data-animation="over-right" data-collapse="medium" data-contain="1" data-doc-height="1" data-duration="400">
+		<div class="w-container">
+			<a class="logo-block w-nav-brand" href="/"><img class="image-logo" sizes="(max-width: 479px) 93vw, (max-width: 767px) 171.109375px, (max-width: 991px) 192.5px, 235.28125px" src="http://uploads.webflow.com/571cab5c86c1049c4c0e7047/5725f0639134bc281e403428_Logo.jpg" srcset="http://uploads.webflow.com/571cab5c86c1049c4c0e7047/5725f0639134bc281e403428_Logo-p-500x117.jpeg 500w, http://uploads.webflow.com/571cab5c86c1049c4c0e7047/5725f0639134bc281e403428_Logo.jpg 800w"></a>
+
+			<nav class="nav-menu w-nav-menu" role="navigation">
+				<div class="dropdown w-dropdown" data-delay="0" data-hover="1">
+				<div class="dropdown-toggle nav-link w-dropdown-toggle">
+				<div>Homepage</div>
+				<div class="dropdown-icon w-icon-dropdown-toggle"></div>
+			</div>
+			<nav class="dropdown-list w-dropdown-list">
+				<a class="dropdown-link w-dropdown-link" href="/">Homepage #1</a>
+				<a class="dropdown-link w-dropdown-link" href="/home-2">Homepage #2</a>
+				<a class="dropdown-link w-dropdown-link" href="/home-3">Homepage #3</a>
+			</nav>
+			</div>
+			<div class="dropdown w-dropdown" data-delay="0" data-hover="1">
+			<div class="dropdown-toggle nav-link w-dropdown-toggle">
+			<div>Courses</div>
+			<div class="dropdown-icon w-icon-dropdown-toggle"></div>
+			</div><nav class="dropdown-list w-dropdown-list">
+				<a class="dropdown-link w-dropdown-link" href="/our-courses/categories">Categories</a>
+				<a class="dropdown-link w-dropdown-link" href="/our-courses/courses-listview">Courses (List View)</a>
+				<a class="dropdown-link w-dropdown-link" href="/our-courses/courses-grid-view">Courses (Grid View)</a>
+				<a class="dropdown-link w-dropdown-link" href="/our-courses/courses-featured">Featured Courses</a>
+				</nav>
+			</div>
+			<div class="dropdown w-dropdown" data-delay="0" data-hover="1">
+				<div class="dropdown-toggle nav-link w-dropdown-toggle">
+				<div>Information</div>
+				<div class="dropdown-icon w-icon-dropdown-toggle"></div>
+			</div>
+			<nav class="dropdown-list w-dropdown-list">
+				<a class="dropdown-link w-dropdown-link" href="/about-us">About us</a>
+				<a class="dropdown-link w-dropdown-link" href="/teachers">Our Teachers</a>
+				<a class="dropdown-link w-dropdown-link" href="/pricing">Pricing Information</a>
+			</nav>
+			</div>
+			<a class="nav-link w-nav-link" href="/events">Events</a>
+			<a class="nav-link w-nav-link" href="/blog">Blog</a>
+			<a class="nav-link w-nav-link" href="/faqs">FAQs</a>
+			<a class="nav-link w-nav-link" href="/contact-us">Contact us</a>
+			</nav>
+			<div class="menu-button w-nav-button">
+				<div class="w-icon-nav-menu"></div>
+			</div>
+			</div>
+			</div>
+			</div>
